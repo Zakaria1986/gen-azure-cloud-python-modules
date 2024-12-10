11b80966e1ee2b0932e30d78cc1e88ec9e057fbe
Zakaria K (AZ-NAT2) <zakariak@genstudents.org>
2024-12-10 18:13:58 +0000

    Second paython task incomplete but will continue during the class time

---
+++ TIFC1-PF-03 - Introducing Lists/exercises/array.py
@@ -0,0 +1,16 @@
+# List of favorite music artists
+names = ["Adele", "Ed Sheeran", "Beyoncé", "Drake", "Taylor Swift"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+# 1. each message should be personalized with the music artist’s name.
+print('\n')
+print("Hello",names[0], "you're music is amazing!")
+print("Hello",names[1], "you're music is amazing!")
+print("Hello",names[2], "you're music is amazing!")
+print("Hello",names[3], "you're music is amazing!")
+print("Hello",names[4], "you're music is amazing!")
