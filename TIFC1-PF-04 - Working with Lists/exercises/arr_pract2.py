971af6314cf7b24107219fccea0190a0e49a23d9
Zakaria K (AZ-NAT2) <zakariak@genstudents.org>
2024-12-13 15:19:47 +0000

    Second practice progress save

--- TIFC1-PF-04 - Working with Lists/exercises/arr_pract2.py
+++ TIFC1-PF-04 - Working with Lists/exercises/arr_pract2.py
@@ -36,15 +36,28 @@ for num1 in range(1,20):
     print('\nNum',num1); 
     
 # 4.  
+    # Generating list of number from 1 to 100 randomly
 #arr_num_list = [random.randint(1,100) for i in range(1,100)]
 arr_num_list = [ ]
 for num in (random.randint(1,100) for i in range(1,100)):
-    arr_num_list.append(num); 
-    
+    arr_num_list.append(num);
+     
+# Printig the list    
 print(arr_num_list)
 
 # Getting the min and max
 min = min(arr_num_list)
 max = max(arr_num_list)
 print('\nmin num:',min,'Max num:', max)
+
+# 5.
+    # 3 from 3 to 30. Use a for loop to print the numbers in your list
+arr_three_times_list = []
+for num in range(3,100,3):
+    #arr_three_times_list.append(num*3)
+    if num <= 30:
+        arr_three_times_list.append(num)
+    
+# Printing out the 3 increments   
+print('\n',arr_three_times_list)
 
