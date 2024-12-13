f08e83631cfd0fce8286a06d07ac99ca99c3c4cf
Zakaria K (AZ-NAT2) <zakariak@genstudents.org>
2024-12-13 11:23:57 +0000

    First practice completed: creating list of pizza array then printing out by looping with additional sentence express my love for coding and pizza

--- TIFC1-PF-04 - Working with Lists/exercises/arr_pract2.py
+++ TIFC1-PF-04 - Working with Lists/exercises/arr_pract2.py
@@ -2,9 +2,17 @@
 
 favorite_pizzas = ["Margherita", "Pepperoni", "BBQ Chicken"]
 
+# 1.
 # looking through with the index for loop
 for i in range(len(favorite_pizzas)):
     print('I like {} Pizza'.format(favorite_pizzas[i]))
-    
+ 
+ #Add a line at the end of your program, The output should consist of three or more lines about the kinds of pizza you like  
+i_love_pizza_string = 'A sentence to expressing my deepest desire for pizza.'
+i_love_pizza_string2 = 'Combining both the coding the eating pizza,'
+i_love_pizza_string3 = 'I confirm i really Enjoy automating expressing my love for pizza by coding my pizza expressions!'
+print ('\n{} {} {}'.format(i_love_pizza_string, i_love_pizza_string2,i_love_pizza_string3))   
+
+# 2.
 
     
