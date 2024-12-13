b59cb09a9058348aba888db7943266e0eca16fc4
Zakaria K (AZ-NAT2) <zakariak@genstudents.org>
2024-12-13 11:04:51 +0000

    Initiating practice for excercise

---
+++ TIFC1-PF-04 - Working with Lists/exercises/arr_pract2.py
@@ -0,0 +1,10 @@
+# Exercise 4: Working with Lists
+
+favorite_pizzas = ["Margherita", "Pepperoni", "BBQ Chicken"]
+
+# looking through with the index for loop
+for i in range(len(favorite_pizzas)):
+    print('I like {} Pizza'.format(favorite_pizzas[i]))
+    
+
+    
