4ff08cb170e0f9810d5ba3d5a5d7d92b3fab85d8
Zakaria K (AZ-NAT2) <zakariak@genstudents.org>
2024-12-17 16:01:53 +0000

    progress on the excercise

--- TIFC1-PF-06 - Dictionaries/exercises/dictionaries-exercises.py
+++ TIFC1-PF-06 - Dictionaries/exercises/dictionaries-exercises.py
@@ -1,55 +1,6 @@
-
-
-## Dynamically do 'CRUD' to the Dictionary
-
-    # Add new pizza into the pizza list
-    
-    # Add new property to each of the pizza 
-
-    # Add toppings to the toppings of each pizza or some of the pizza's
-
-## Make this more interactive by adding input for example, let users input the data in console and then added to pizza list
- 
- # Check to see if input already exist in the list if not add
- 
- # Choose from option which perform CRUD on i.e., add new pizza, add new toppings or add a new property to the pizza's
-
-
-## Finally print out all the result based on customer option:
- 
- # allow list of option from the menue i.e., Pizza's 
- 
- # Based on the input show the properties of that Pizza
- 
- # if they want to order the pizza calculate and show the total price and end the program. 
-
-
-# ['Margarita','peperoni','fungi']
-pizza_menu = [
-                {'Margarita':{
-                    'name' : 'Margarita',
-                    'price': 5,
-                    'size' : '8 inch',
-                    'toppings': ['cheese','tomato','basil']
-                    }
-                },
-                
-                {'peperoni':{
-                    'name' : 'Peperoni',
-                    'price' : 6,
-                    'size': '8',  
-                    'toppings': ['cheese','tomato', 'salami']
-                    }
-                },
-                {'fungi':{
-                    'name' : 'Fungi' ,
-                    'price' : 7,
-                    'size' : '8 inch',
-                    'toppings': ['cheese','tomato','mushroom']
-                    }
-                }
-            
-         ]
+from dictionary_list import pizza_menu
+from functions import get_total_num_pizz
+from functions import get_user_selected_pizza
 
 hawaiian_properties =  {
          'name' : "Hawaiian",
@@ -60,13 +11,10 @@ hawaiian_properties.update({'toppings': [ 'Ham', 'pineapple', 'mozzarella cheese
 hawaiian =  {'hawaiian': hawaiian_properties }
 pizza_menu.append(hawaiian)
  
- #  add discription seperatly to the list:   "description": "Ham and pineapple with tomato sauce and mozzarella."
+# add discription seperatly to the list:   "description": "Ham and pineapple with tomato sauce and mozzarella."
 
-# being lazing: don't wanna change the names below
 pizza = pizza_menu
-margherita_properties = len(pizza)
-print('total number of pizza in the Dictionary', margherita_properties) 
-#print(pizza)
+
 
 # String to inlcude inside the input
 pizza_name = ''
@@ -95,20 +43,10 @@ for piz in pizza:
 
 print('\n',pizza_name_list)
 
-def get_user_selected_pizza(input, pizza_list):
-    ## Use it if statement to match the names
-    output = ''
-    
-    for piz in pizza_list:
-        for key, val in piz.items():  
-            toppins = ''
-            if val['name'].lower() == input:
-                output += f'Pizza name: {val['name']}, \nSize: {val['size']},\nPrice: £{val['price']},\nToppings:'
-                for index, topins in enumerate(val['toppings']):
-                    toppins += f'\n\t{index} = {topins} ' 
-        output+=toppins
-    return output; 
-        
+## Functions saved as variable for later use
+
+# Get the length of pizza list 
+totalNum = get_total_num_pizz(pizza)
        
 # check for correct entry or otherwise ask for the correct entry
 while True:
@@ -119,29 +57,15 @@ while True:
     if choice == 'exit':
         break  
     elif not choice.isalpha() or choice not in pizza_name_list:
-        print("\n\n\Invalid input. Please enter a valid option from the menu. '\n\n\t\tOr enter \'exit\' to exit the console'")   
+        print("\n\nInvalid input. Please enter a valid option from the menu. '\n\n\t\tOr enter \'exit\' to exit the console'")   
   
     else:
         print('\nYou have entered {}'.format(get_user_selected_pizza(choice, pizza_menu)))
-        
+        print('\nTotal number of pizza avaiable: ',totalNum)
         print('\n\t\tEnter \'exit\' to exit the console')
     
-    
-       
-     
-    
-        
-        
-        
-    
-
-    # if input_op  == "Fungi":
-    #     print('correct getting your informaiton')
-    # else:
-    #     print("Incorrect input, select one from our menu list:")
 
 
-## write all the else if statements logics here to only vew whats been selected
 
             
             
---
+++ TIFC1-PF-06 - Dictionaries/exercises/dictionary_list.py
@@ -0,0 +1,54 @@
+
+
+# ['Margarita','peperoni','fungi']
+pizza_menu = [
+                {'Margarita':{
+                    'name' : 'Margarita',
+                    'price': 5,
+                    'size' : '8 inch',
+                    'toppings': ['cheese','tomato','basil']
+                    }
+                },
+                
+                {'peperoni':{
+                    'name' : 'Peperoni',
+                    'price' : 6,
+                    'size': '8',  
+                    'toppings': ['cheese','tomato', 'salami']
+                    }
+                },
+                {'fungi':{
+                    'name' : 'Fungi' ,
+                    'price' : 7,
+                    'size' : '8 inch',
+                    'toppings': ['cheese','tomato','mushroom']
+                    }
+                }   
+         ]
+
+
+
+
+
+## Dynamically do 'CRUD' to the Dictionary
+
+    # Add new pizza into the pizza list
+    
+    # Add new property to each of the pizza 
+
+    # Add toppings to the toppings of each pizza or some of the pizza's
+
+## Make this more interactive by adding input for example, let users input the data in console and then added to pizza list
+ 
+ # Check to see if input already exist in the list if not add
+ 
+ # Choose from option which perform CRUD on i.e., add new pizza, add new toppings or add a new property to the pizza's
+
+
+## Finally print out all the result based on customer option:
+ 
+ # allow list of option from the menue i.e., Pizza's 
+ 
+ # Based on the input show the properties of that Pizza
+ 
+ # if they want to order the pizza calculate and show the total price and end the program. 
